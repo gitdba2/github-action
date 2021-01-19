fbee1a3d5269ad50f125f421f4fe73b0ba1a8205
Tom <odeatom@gmail.com>
2021-01-19 16:29:00 -0600

    first

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    subnet_id = "subnet-092d9022b53030041"
+    vpc_security_group_ids = [ "sg-0eaf1c28c7f786ea6" ]
+    ami = "ami-04d29b6f966df1537"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "mytag"
+    }
+}
