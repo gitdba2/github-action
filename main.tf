c37ed856846273e34e5e86abfdf50986f3e75e86
Tom <odeatom@gmail.com>
2021-01-19 16:39:24 -0600

    mainchanges

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
     subnet_id = "subnet-092d9022b53030041"
     vpc_security_group_ids = [ "sg-0eaf1c28c7f786ea6" ]
