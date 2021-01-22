d4ccd162c5d70d77f0f5e9e92389505197e9f447
Tom <odeatom@gmail.com>
2021-01-21 18:48:41 -0600

    terraform version now 14.5

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 #  Configure the AWS Provider                                                  #
 ## =============================================================================
 terraform {
-  required_version = "= 0.14.4"
+  required_version = "= 0.14.5"
 
   required_providers {
     aws = "~> 3.24"
