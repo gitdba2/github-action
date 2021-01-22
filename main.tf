c013bd6af97711bade0a40c56b78d4c66a1764fa
Tom <odeatom@gmail.com>
2021-01-22 13:55:31 -0600

    add variable for SG

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 
 resource "aws_instance" "example" {
-  subnet_id              = "subnet-092d9022b53030041"
-  vpc_security_group_ids = ["sg-0eaf1c28c7f786ea6"]
+  subnet_id = "subnet-092d9022b53030041"
+  #vpc_security_group_ids = ["sg-0eaf1c28c7f786ea6"]
+  vpc_security_group_ids = [aws_security_group.mySG]
   ami                    = "ami-04d29b6f966df1537"
   instance_type          = "t2.micro"
 
